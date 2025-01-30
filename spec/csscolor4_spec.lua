25d47b3c27e37a438edcfb88836061f74d47dae1
Louis Chan <louischan0325@gmail.com>
2025-01-30 20:28:52 +0800

    Support colorspace prophoto-rgb

--- spec/csscolor4_spec.lua
+++ spec/csscolor4_spec.lua
@@ -673,3 +673,53 @@ describe("a98_rgb", function()
     assert.same_color({ "a98-rgb", { -0.2, -0.4, -0.6 }, 0 }, csscolor4.a98_rgb("-20%", "-40%", "-60%", "-0.1"))
   end)
 end)
+
+describe("prophoto_rgb", function()
+  it("parse prophoto_rgb", function()
+    assert.same_color(nil, csscolor4.prophoto_rgb("", "", ""))
+
+    assert.same_color({ "prophoto-rgb", { "none", "none", "none" } }, csscolor4.prophoto_rgb("none", "none", "none"))
+    assert.same_color(
+      { "prophoto-rgb", { "none", "none", "none" }, "none" },
+      csscolor4.prophoto_rgb("none", "none", "none", "none")
+    )
+
+    assert.same_color({ "prophoto-rgb", { "none", "none", "none" } }, csscolor4.prophoto_rgb("NONE", "NONE", "NONE"))
+    assert.same_color(
+      { "prophoto-rgb", { "none", "none", "none" }, "none" },
+      csscolor4.prophoto_rgb("NONE", "NONE", "NONE", "NONE")
+    )
+
+    assert.same_color({ "prophoto-rgb", { 0, 0, 0 } }, csscolor4.prophoto_rgb("0", "0", "0"))
+    assert.same_color({ "prophoto-rgb", { 1, 1, 1 } }, csscolor4.prophoto_rgb("1", "1", "1"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 } }, csscolor4.prophoto_rgb("0.2", "0.4", "0.6"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 }, 0.5 }, csscolor4.prophoto_rgb("0.2", "0.4", "0.6", "50%"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 }, 0.5 }, csscolor4.prophoto_rgb("0.2", "0.4", "0.6", "0.5"))
+    assert.same_color({ "prophoto-rgb", { 1.2, 1.4, 1.6 }, 1 }, csscolor4.prophoto_rgb("1.2", "1.4", "1.6", "120%"))
+    assert.same_color({ "prophoto-rgb", { -0.2, -0.4, -0.6 } }, csscolor4.prophoto_rgb("-0.2", "-0.4", "-0.6"))
+    assert.same_color(
+      { "prophoto-rgb", { -0.2, -0.4, -0.6 }, 0 },
+      csscolor4.prophoto_rgb("-0.2", "-0.4", "-0.6", "-10%")
+    )
+    assert.same_color(
+      { "prophoto-rgb", { -0.2, -0.4, -0.6 }, 0 },
+      csscolor4.prophoto_rgb("-0.2", "-0.4", "-0.6", "-0.1")
+    )
+
+    assert.same_color({ "prophoto-rgb", { 0, 0, 0 } }, csscolor4.prophoto_rgb("0%", "0%", "0%"))
+    assert.same_color({ "prophoto-rgb", { 1, 1, 1 } }, csscolor4.prophoto_rgb("100%", "100%", "100%"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 } }, csscolor4.prophoto_rgb("20%", "40%", "60%"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 }, 0.5 }, csscolor4.prophoto_rgb("20%", "40%", "60%", "50%"))
+    assert.same_color({ "prophoto-rgb", { 0.2, 0.4, 0.6 }, 0.5 }, csscolor4.prophoto_rgb("20%", "40%", "60%", "0.5"))
+    assert.same_color({ "prophoto-rgb", { 1.2, 1.4, 1.6 }, 1 }, csscolor4.prophoto_rgb("120%", "140%", "160%", "120%"))
+    assert.same_color({ "prophoto-rgb", { -0.2, -0.4, -0.6 } }, csscolor4.prophoto_rgb("-20%", "-40%", "-60%"))
+    assert.same_color(
+      { "prophoto-rgb", { -0.2, -0.4, -0.6 }, 0 },
+      csscolor4.prophoto_rgb("-20%", "-40%", "-60%", "-10%")
+    )
+    assert.same_color(
+      { "prophoto-rgb", { -0.2, -0.4, -0.6 }, 0 },
+      csscolor4.prophoto_rgb("-20%", "-40%", "-60%", "-0.1")
+    )
+  end)
+end)
