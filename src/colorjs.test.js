379b8ee3a87bceea98a4918d33c497a1e6389540
Louis Chan <louischan0325@gmail.com>
2025-10-25 00:09:25 +0800

    Remove Nodejs implementation

--- src/colorjs.test.js
+++
@@ -1,8 +0,0 @@
-const Color = require("colorjs.io").default;
-
-describe("Color", () => {
-  it("should treat none case-insensitively, but it does not", () => {
-    expect((new Color("RGB(None none none)")).toString({format: "hex", collapse: false})).toEqual("#NaNNaNNaN");
-    expect(new Color("RGB(none none none)").toString({format: "hex", collapse: false})).toEqual("#000000");
-  })
-});
