48b9a6dde08d39abc62dda41cf1753ce962aeb92
Louis Chan <louischan0325@gmail.com>
2026-04-02 02:22:05 +0800

    Use on_range from Neovim 0.12

--- lua/nvim-colors/treesitter.lua
+++ lua/nvim-colors/treesitter.lua
@@ -176,98 +176,11 @@ local function tsnode_to_color(buf, capture_name, tsnode, tw_theme_colors, text)
   end
 end
 
----@param range1 Range4
----@param range2 Range4
----@return boolean
-local function range4_contains(range1, range2)
-  local result = range1[1] <= range2[1] and range1[2] <= range2[2] and range1[3] >= range2[3] and range1[4] >= range2[4]
-  return result
-end
-
---- @param bufnr integer
---- @param row integer
---- @return integer
-local function get_byte_count_in_row(bufnr, row)
-  local byte_count_including_eol = vim.api.nvim_buf_get_offset(bufnr, row + 1) - vim.api.nvim_buf_get_offset(bufnr, row)
-  return byte_count_including_eol
-end
-
----@param winid integer
----@param bufnr integer
----@param toprow integer
----@param botrow integer
----@return { [integer]: Range4 }
-local function on_win_impl(winid, bufnr, toprow, botrow)
-  local getwininfo_result = vim.fn.getwininfo(winid)[1]
-  local width = getwininfo_result.width - getwininfo_result.textoff
-  local height = getwininfo_result.height
-  local cell_count = width * height
-
-  -- leftcol is returned in getwininfo on Neovim >= 0.11
-  local leftcol = 0
-  if vim.api.nvim_get_current_win() == winid then
-    local winsaveview_result = vim.fn.winsaveview()
-    leftcol = winsaveview_result.leftcol
-  end
-
-  local cursor_position_ = vim.api.nvim_win_get_cursor(winid)
-  local cursor_position = { cursor_position_[1] - 1, cursor_position_[2] }
-  local wrap = vim.wo[winid].wrap
-
-  -- UTF-8 character is at most 4-byte.
-  local multiplier = 4
-
-  --- @type Range4[]
-  local line_range_list = {}
-  --- @type { [integer]: Range4 }
-  local line_range_table = {}
-
-  for row = toprow, botrow do
-    local byte_count = get_byte_count_in_row(bufnr, row)
-
-    local line_range = nil
-    if wrap then
-      -- If it is not the cursor row, the cursor is like at the 0 column.
-      local cursor_col = 0
-
-      local is_cursor_row = cursor_position[1] == row
-      if is_cursor_row then
-        -- It is the cursor row, we need to consider the horizontal scroll offset.
-        cursor_col = cursor_position[2]
-      end
-
-      -- Assuming the cursor is at the bottom-right of the screen,
-      -- the byte at the top-left of the screen is cursor_col - 4-byte * cell_count
-      local minimum_start_col = math.max(0, cursor_col - multiplier * cell_count)
-      -- Assuming the cursor is at the top-left of the screen,
-      -- the byte at the bottom-right of the screen is cursor_col + 4-byte * cell_count
-      local maximum_end_col = math.min(byte_count, cursor_col + multiplier * cell_count)
-      line_range = { row, minimum_start_col, row, maximum_end_col }
-    else
-      -- Assume the worst case.
-      -- All characters are 4-byte and they are shown as 1 cell.
-      -- To fill the whole line, we need width * 4 bytes.
-      local worst_case_byte_count = multiplier * width
-      line_range = {
-        row,
-        math.max(0, leftcol - worst_case_byte_count),
-        row,
-        math.min(byte_count, (leftcol + worst_case_byte_count)),
-      }
-    end
-
-    table.insert(line_range_list, line_range)
-    line_range_table[row] = line_range
-  end
-
-  return line_range_table
-end
-
 ---@class TreesitterViewport
 ---@field fg color
 ---@field bg color
 ---@field bufnr integer
----@field line_range Range4
+---@field range_end_exclusive Range4
 local TreesitterViewport = {}
 
 --- @class (exact) NewTreesitterHighlighterOptions
@@ -309,51 +222,20 @@ function TreesitterHighlighter:destroy()
   self.ltree:destroy()
 end
 
---- @param winid integer
---- @param toprow integer
---- @param botrow integer
-function TreesitterHighlighter:on_win(winid, toprow, botrow)
-  self.viewport[winid] = {}
-  if self.enabled then
-    local fg, bg = css.get_fg_bg_from_colorscheme()
-    local line_ranges = on_win_impl(winid, self.bufnr, toprow, botrow)
-    for row, line_range in pairs(line_ranges) do
-      self.viewport[winid][row] = {
-        fg = fg,
-        bg = bg,
-        bufnr = self.bufnr,
-        line_range = line_range,
-      }
-    end
-  end
-end
-
---- @param winid integer
---- @param row integer
-function TreesitterHighlighter:on_line(winid, row)
-  local a = self.viewport[winid]
-  if a ~= nil then
-    local viewport = a[row]
-    if viewport ~= nil then
-      self:_highlight_viewport(viewport)
-    end
-  end
-end
-
 --- @param viewport TreesitterViewport
-function TreesitterHighlighter:_highlight_viewport(viewport)
+function TreesitterHighlighter:on_range(viewport)
   local query = self.query
   local ltree = self.ltree
   local ns = self.ns
   local tw_theme_colors = self.tw_theme_colors
 
-  -- NOTE: :parse() is very slow on a file like this,
+  -- NOTE: :parse() could be slow on a file like this,
   -- even the given range is small.
   -- https://github.com/justinmk/notes/blob/master/delicious.md
   -- https://github.com/neovim/neovim/issues/22426
-  local root = ltree:parse(viewport.line_range)[1]:root()
+  local root = ltree:parse(viewport.range_end_exclusive)[1]:root()
 
-  -- NOTE: This is slow on very long line.
+  -- NOTE: iter_captures could be slow on long lines.
   -- https://github.com/neovim/neovim/issues/22426
   -- https://github.com/neovim/neovim/issues/14756
   -- https://github.com/neovim/neovim/pull/15405
@@ -362,39 +244,36 @@ function TreesitterHighlighter:_highlight_viewport(viewport)
     query:iter_captures(
       root,
       viewport.bufnr,
-      viewport.line_range[1],
-      viewport.line_range[3],
-      { start_col = viewport.line_range[2], end_col = viewport.line_range[4] }
+      viewport.range_end_exclusive[1],
+      viewport.range_end_exclusive[3],
+      { start_col = viewport.range_end_exclusive[2], end_col = viewport.range_end_exclusive[4] }
     )
   do
     local capture_name = query.captures[id]
     local start_row, start_col, end_row, end_col = node:range()
-    local node_range = { start_row, start_col, end_row, end_col }
 
-    if range4_contains(viewport.line_range, node_range) then
-      local text = vim.treesitter.get_node_text(node, viewport.bufnr)
-      local css_color = tsnode_to_color(viewport.bufnr, capture_name, node, tw_theme_colors, text)
-      if css_color ~= nil then
-        local result = css.convert_css_color({
-          color = css_color,
-          fg_color = viewport.fg,
-          bg_color = viewport.bg,
+    local text = vim.treesitter.get_node_text(node, viewport.bufnr)
+    local css_color = tsnode_to_color(viewport.bufnr, capture_name, node, tw_theme_colors, text)
+    if css_color ~= nil then
+      local result = css.convert_css_color({
+        color = css_color,
+        fg_color = viewport.fg,
+        bg_color = viewport.bg,
+      })
+      if result then
+        local hl_group = get_nvim_hl_group_name(result)
+        -- Based on observation, we do not cache the calls to nvim_set_hl()
+        -- It must be called in each draw cycle.
+        vim.api.nvim_set_hl(ns, hl_group, {
+          fg = result.highlight_fg,
+          bg = result.highlight_bg,
         })
-        if result then
-          local hl_group = get_nvim_hl_group_name(result)
-          -- Based on observation, we do not cache the calls to nvim_set_hl()
-          -- It must be called in each draw cycle.
-          vim.api.nvim_set_hl(ns, hl_group, {
-            fg = result.highlight_fg,
-            bg = result.highlight_bg,
-          })
-          vim.api.nvim_buf_set_extmark(viewport.bufnr, ns, start_row, start_col, {
-            end_row = end_row,
-            end_col = end_col,
-            hl_group = hl_group,
-            ephemeral = true,
-          })
-        end
+        vim.api.nvim_buf_set_extmark(viewport.bufnr, ns, start_row, start_col, {
+          end_row = end_row,
+          end_col = end_col,
+          hl_group = hl_group,
+          ephemeral = true,
+        })
       end
     end
   end
@@ -476,27 +355,23 @@ function M.setup()
   })
 
   vim.api.nvim_set_decoration_provider(ns, {
-    on_win = function(_, winid, bufnr, toprow, botrow)
-      -- nvim_set_hl_ns_fast() is intended to be used with nvim_set_decoration_provider().
-      -- With nvim_set_decoration_provider() and nvim_set_hl_ns_fast(),
-      -- calling nvim_set_hl_ns() and nvim_win_set_hl_ns() are unnecessary.
+    on_win = function()
       vim.api.nvim_set_hl_ns_fast(ns)
-
-      -- Return false in on_win will skip on_line, but
-      -- retain the extmarks created in a previous call.
-      -- We want to behavior that if we do not call nvim_buf_set_extmark()
-      -- then we expect no extmarks to be drawn.
-      -- So we never return false in on_win or on_line.
-
-      local highlighter = highlighters[bufnr]
-      if highlighter ~= nil then
-        highlighter:on_win(winid, toprow, botrow)
-      end
     end,
-    on_line = function(_, winid, bufnr, row)
+    -- on_range was introduced in Neovim 0.12
+    -- Its primary user is the builtin treesitter highlight.
+    -- It was an optimization over the deprecated on_line.
+    -- See https://github.com/neovim/neovim/pull/31400
+    on_range = function(_, _winid, bufnr, begin_row, begin_col, end_row, end_col)
       local highlighter = highlighters[bufnr]
       if highlighter ~= nil then
-        highlighter:on_line(winid, row)
+        local fg, bg = css.get_fg_bg_from_colorscheme()
+        highlighter:on_range({
+          fg = fg,
+          bg = bg,
+          bufnr = bufnr,
+          range_end_exclusive = { begin_row, begin_col, end_row, end_col },
+        })
       end
     end,
   })
